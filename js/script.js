2fb2fcd514cc09df9407eafd59444fc2a2df8cfd
Ricardo Pineda <richipineda2@gmail.com>
2021-02-13 22:42:21 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 var urlApi = "https://webapp-210130211157.azurewebsites.net/webresources/mitienda/";
+var requestobj;
 
 fetch("https://webapp-210130211157.azurewebsites.net/webresources/mitienda/")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+      for (var itr = 0; itr<data.length; itr++){
+          console.log(data[0]);
+          console.log("--------------------------------------------")
+          console.log(data[itr].idTienda);
+      };
+  });
