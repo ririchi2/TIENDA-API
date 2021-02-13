a1edda1d4732c7b8fbbc9e2e0f135b356bad6cd2
Ricardo Pineda <richipineda2@gmail.com>
2021-02-13 23:13:17 +0000

    Update script.js
    - added metodos createNode and removeChilds

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 var urlApi = "https://webapp-210130211157.azurewebsites.net/webresources/mitienda/";
 var requestobj;
 
+//METODOS
+function createNode(nodeName, nodeText, nodeClasses, nodeAttributes) {
+    var node = document.createElement(nodeName);
+
+    if(nodeText!="") {
+        var textNode = document.createTextNode(nodeText);
+        node.appendChild(textNode);
+    }
+
+    if(nodeClasses.length > 0) {
+        nodeClasses.forEach(nodeClass => node.classList.add(nodeClass));
+    }
+
+    if(nodeAttributes.length > 0) {
+        nodeAttributes.forEach(nodeAttribute => node.setAttribute(nodeAttribute.name, nodeAttribute.value));
+    }
+
+    return node;
+}
+function removeChilds(node) {
+    var childs = Array.from(node.childNodes);
+
+    childs.forEach(child => child.remove());
+}
+
 fetch("https://webapp-210130211157.azurewebsites.net/webresources/mitienda/")
   .then(response => response.json())
   .then(data => {
