d268347d438779c39a95c67464b703ea156b9357
Ricardo Pineda <richipineda2@gmail.com>
2021-02-13 05:23:05 +0000

    gulp sass still doesnt work

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
 'use strict'
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', function(){
-    return gulp.src("./css/styles.scss")
+    return gulp.src("./css/*.scss")
       .pipe(sass().on('error', sass.logError)) // Converts Sass to CSS with gulp-sass
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
 });
 
 gulp.task('browserSync', function(){
     browserSync.init({
-        server: "./"
+        server: {
+            baseDir: "./"
+        }
     })
 });
 
 
 // Static Server + watching scss/html/js files
-gulp.task('serve', gulp.series(['sass']), function() {
-
+gulp.task('watch', gulp.series(['sass']), function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-
-    gulp.watch('./css/*.scss', gulp.series(['sass']));
+    gulp.watch('./css/styles.scss', gulp.series(['sass']));
     gulp.watch("./*.html").on('change', browserSync.reload);
     gulp.watch("./js/*.js").on('change', browserSync.reload);
-
 });
