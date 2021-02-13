eba076ded10d78b8a79a8010dc7d53a91248fe70
Ricardo Pineda <richipineda2@gmail.com>
2021-02-13 22:03:58 +0000

    jquery add, and fetch request testing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 var urlApi = "https://webapp-210130211157.azurewebsites.net/webresources/mitienda/";
+
+fetch("https://webapp-210130211157.azurewebsites.net/webresources/mitienda/")
+  .then(response => response.json())
+  .then(data => console.log(data));
