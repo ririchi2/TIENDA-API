517f54050dfabf19d2023a95f6c5888a1da843b2
Ricardo Pineda <richipineda2@gmail.com>
2021-02-14 05:59:29 +0000

    various changes
    - added Fetch GET petition

--- js/script.js
+++ js/script.js
@@ -1,40 +1,80 @@
-var urlApi = "https://webapp-210130211157.azurewebsites.net/webresources/mitienda/";
-var requestobj;
+// var urlApi = "https://webapp-210130211157.azurewebsites.net/webresources/mitienda/";
 
-//METODOS
+// Containers
+const mainContainer = document.getElementsByTagName("main")[0];
+const indexContainer = document.getElementsByClassName("index")[0];
+const loadingContainer = document.getElementsByClassName("loading")[0];
+const resultContainer = document.getElementsByClassName("result")[0];
+
+// Opciones de la peticion
+var myHeaders = new Headers();
+var options = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default'
+};
+
+const btnFetch = document.getElementById("btnFetch");
+
+btnFetch.addEventListener("click", () => {
+    indexContainer.style.display = "none";
+    loadingContainer.style.display = "";
+    fetch("https://webapp-210130211157.azurewebsites.net/webresources/mitienda/", options)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.log("Respuesta de red OK pero respuesta HTTP no OK");
+                indexContainer.style.display = "";
+                loadingContainer.style.display = "none";
+            }
+        })
+        .then(data => {
+            data.forEach(data => {
+                loadingContainer.style.display = "none";
+                resultContainer.style.display = "";
+                //   console.log(data.idTienda);
+
+                var cardContainer = createNode("div", "", ["card"], []);
+                resultContainer.appendChild(cardContainer);
+                var h2Card = createNode("h2", data.nombreTienda, [], []);
+                cardContainer.appendChild(h2Card);
+                cardContainer.appendChild(createNode("p", `${data.direccion} (${data.localidad})`, [], []));
+                cardContainer.appendChild(createNode("p", data.telefono, [], []));
+            });
+        })
+        .catch(function (error) {
+            console.log("Hubo un problema con la peticion Fetch: " + error.message);
+            indexContainer.style.display = "";
+            loadingContainer.style.display = "none";
+        })
+
+});
+
+
+// METODOS
 function createNode(nodeName, nodeText, nodeClasses, nodeAttributes) {
     var node = document.createElement(nodeName);
 
-    if(nodeText!="") {
+    if (nodeText != "") {
         var textNode = document.createTextNode(nodeText);
         node.appendChild(textNode);
     }
 
-    if(nodeClasses.length > 0) {
+    if (nodeClasses.length > 0) {
         nodeClasses.forEach(nodeClass => node.classList.add(nodeClass));
     }
 
-    if(nodeAttributes.length > 0) {
+    if (nodeAttributes.length > 0) {
         nodeAttributes.forEach(nodeAttribute => node.setAttribute(nodeAttribute.name, nodeAttribute.value));
     }
 
     return node;
 }
+
 function removeChilds(node) {
     var childs = Array.from(node.childNodes);
 
     childs.forEach(child => child.remove());
 }
-
-fetch("https://webapp-210130211157.azurewebsites.net/webresources/mitienda/")
-  .then(response => response.json())
-  .then(data => {
-      data.forEach(data => {
-          console.log(data.idTienda)
-      })
-    //   for (var itr = 0; itr<data.length; itr++){
-    //       console.log(data[0]);
-    //       console.log("--------------------------------------------")
-    //       console.log(data[itr].idTienda);
-    //   };
-  });
